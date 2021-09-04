cb68658d86e3f705124f3352d2186f2bcbacb5c8
reza sanjari <sanjarireza1996@gmail.com>
2021-09-03 22:13:09 -0700

    add changes

---
+++ src/components/water/index.tsx
@@ -0,0 +1,20 @@
+import './index.css'
+
+import { Button } from 'reactstrap';
+
+
+
+function Water() {
+
+    return (
+       <div className="drops"> 
+       hello my name is reza sanjari
+       <div className="drop"> </div>
+       <div className="drop"> </div>
+       <div className="drop"> </div>
+       <div className="drop"> </div>
+       </div>
+    );
+    
+}
+export default Water;
